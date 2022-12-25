822ce4f5a9a8bad1ad449605db1ecff91e6769b8
Rubén Abrante Delgado <abrantedr@gmail.com>
2022-12-25 13:49:55 +0000

    First version of REE app.

---
+++ src/Api.java
@@ -0,0 +1,169 @@
+import org.jfree.data.time.Minute;
+import org.jfree.data.time.TimeSeries;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
+import java.io.*;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+
+
+public class Api {
+    protected final static String path = "G:\\Mi unidad\\Master en Ingenieria Industrial\\" +
+                "Tercer Cuatrimestre\\Informática Industrial\\Practicas\\" +
+                "REE\\data\\data.json";
+    protected static TimeSeries demSeries = new TimeSeries("Demanda");
+    protected static TimeSeries dieSeries = new TimeSeries("Diésel");
+    protected static TimeSeries gasSeries = new TimeSeries("Turbina de gas");
+    protected static TimeSeries eolSeries = new TimeSeries("Eólica");
+    protected static TimeSeries ccSeries = new TimeSeries("Ciclo combinado");
+    protected static TimeSeries vapSeries = new TimeSeries("Turbina de vapor");
+    protected static TimeSeries fotSeries = new TimeSeries("Solar fotovoltaica");
+    protected static TimeSeries hidSeries = new TimeSeries("Hidráulica");
+
+    public Api() {
+        // Do nothing
+    }
+
+    public static void main(String[] args) {
+        // Do nothing
+    }
+
+    public static void listContent(final String filePath) throws Exception {
+        java.util.Date ts = null;
+        double
+                dem = 0,
+                die = 0,
+                gas = 0,
+                eol = 0,
+                cc = 0,
+                vap = 0,
+                fot = 0,
+                hid = 0;
+
+        // Define the date format to use for parsing
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm"
+        );
+
+        try (InputStream is = new FileInputStream(filePath);
+             JsonParser parser = Json.createParser(is)) {
+            while (parser.hasNext()) {
+                Event e = parser.next();
+                if (e == Event.KEY_NAME) {
+                    switch (parser.getString()) {
+                        case "ts" -> {
+                            parser.next();
+                            // Get timestamp
+                            String stringts = parser.getString();
+                            ts = dateFormat.parse(stringts);
+                        }
+                        case "dem" -> {
+                            parser.next();
+                            // Get demand
+                            String stringdem = parser.getString();
+                            dem = Double.parseDouble(stringdem);
+                        }
+                        case "die" -> {
+                            parser.next();
+                            // Get die
+                            String stringdie = parser.getString();
+                            die = Double.parseDouble(stringdie);
+                        }
+                        case "gas" -> {
+                            parser.next();
+                            // Get gas
+                            String stringgas = parser.getString();
+                            gas = Double.parseDouble(stringgas);
+                        }
+                        case "eol" -> {
+                            parser.next();
+                            // Get eol
+                            String stringeol = parser.getString();
+                            eol = Double.parseDouble(stringeol);
+                        }
+                        case "cc" -> {
+                            parser.next();
+                            // Get cc
+                            String stringcc = parser.getString();
+                            cc = Double.parseDouble(stringcc);
+                        }
+                        case "vap" -> {
+                            parser.next();
+                            // Get vap
+                            String stringvap = parser.getString();
+                            vap = Double.parseDouble(stringvap);
+                        }
+                        case "fot" -> {
+                            parser.next();
+                            // Get fot
+                            String stringfot = parser.getString();
+                            fot = Double.parseDouble(stringfot);
+                        }
+                        case "hid" -> {
+                            parser.next();
+                            // Get hid
+                            String stringhid = parser.getString();
+                            hid = Double.parseDouble(stringhid);
+
+                            // At this point, we've gathered all the data
+                            // Now it's time to add to time series
+                            Minute tsDate = new Minute(ts);
+                            demSeries.addOrUpdate(tsDate, dem);
+                            dieSeries.addOrUpdate(tsDate, die);
+                            gasSeries.addOrUpdate(tsDate, gas);
+                            eolSeries.addOrUpdate(tsDate, eol);
+                            ccSeries.addOrUpdate(tsDate, cc);
+                            vapSeries.addOrUpdate(tsDate, vap);
+                            fotSeries.addOrUpdate(tsDate, fot);
+                            hidSeries.addOrUpdate(tsDate, hid);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static final String getUrl(final String date) {
+        return "https://demanda.ree.es/WSvisionaMovilesCanariasRest/" +
+                "resources/demandaGeneracionCanarias?" +
+                "callback=angular.callbacks._2&curva=CANARIAS&" +
+                "fecha=" + date;
+    }
+
+    public static void download(final String url, final String filePath) throws
+            Exception {
+        URL urlFile = new URL(url);
+        InputStream inputStream = urlFile.openStream();
+        OutputStream outputStream = new FileOutputStream(filePath);
+
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(inputStream));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        sb.delete(0, 21);
+        int length = sb.length();
+        sb.delete(length - 2, length);
+
+        outputStream.write(sb.toString().getBytes());
+        inputStream.close();
+        outputStream.close();
+    }
+}
---
+++ src/Gui.java
@@ -0,0 +1,445 @@
+import io.loli.datepicker.DatePicker;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.TimeSeriesCollection;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+
+
+public class Gui implements ActionListener {
+    private final Api api;
+    private TimeSeriesCollection data;
+    private String fromDate, toDate;
+    private JFrame frame;
+    private ChartPanel chart;
+    private JPanel buttonPanel, datePickerPanel, chartPanel;
+    private JToggleButton demButton, dieButton, gasButton, eolButton, ccButton,
+            vapButton, fotButton, hidButton;
+
+
+    public Gui() {
+        // Create API interface object
+        api = new Api();
+
+
+        // Create GUI components
+        createButtonPanel();
+        createDatePicker();
+
+        // First API call
+        makeApiCall();
+
+        // Add chart
+        createChart();
+
+        // Create main panel and add components
+        createMainPanel();
+    }
+
+    public static void main(String[] args) {
+        Gui frontend = new Gui();
+    }
+
+    public void createMainPanel() {
+        // Create main frame
+        frame = new JFrame("Red Eléctrica España");
+
+        // Create a grid layout for the main frame
+        GridBagLayout grid = new GridBagLayout();
+        frame.setLayout(grid);
+
+        // Create a GridBagConstraints object to specify the constraints
+        // for each panel
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        // Add the top panel to the JFrame with the specified constraints
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.weightx = 1;
+        constraints.weighty = 0.5;
+        constraints.fill = GridBagConstraints.BOTH;
+        frame.add(chartPanel, constraints);
+
+        // Add the first (left) bottom panel to the JFrame with the
+        // specified constraints
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.weighty = 0.25;
+        frame.add(buttonPanel, constraints);
+
+        // Add the second (right) bottom panel to the JFrame with the
+        // specified constraints
+        constraints.gridx = 1;
+        frame.add(datePickerPanel, constraints);
+
+        // Set frame options
+        frame.setLocation(200, 200);
+        frame.setVisible(true);
+        frame.setSize(860, 750);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createButtonPanel() {
+        // Create a grid layout for the toggle buttons
+        GridLayout grid = new GridLayout(4, 2, 5, 7);
+
+        // Create button panel
+        buttonPanel = new JPanel(grid);
+
+        // Create toggle buttons
+        demButton = new JToggleButton("Demanda total", true);
+        dieButton = new JToggleButton("Generación con motores diésel");
+        gasButton = new JToggleButton("Generación con turbinas de gas");
+        eolButton = new JToggleButton("Generación eólica");
+        ccButton = new JToggleButton("Generación en ciclos combinados");
+        vapButton = new JToggleButton("Generación en ciclo de vapor");
+        fotButton = new JToggleButton("Generación fotovoltaica");
+        hidButton = new JToggleButton("Generación hidráulica");
+
+        // Add buttons to button panel
+        buttonPanel.add(demButton);
+        buttonPanel.add(dieButton);
+        buttonPanel.add(gasButton);
+        buttonPanel.add(eolButton);
+        buttonPanel.add(ccButton);
+        buttonPanel.add(vapButton);
+        buttonPanel.add(fotButton);
+        buttonPanel.add(hidButton);
+
+        // Set a border for button panel
+        buttonPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK),
+                BorderFactory.createEmptyBorder(10, 15, 5, 15)));
+
+        // Set action listeners for each button
+        demButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent demButton) {
+                try {
+                    JToggleButton demButtonState =
+                            (JToggleButton) demButton.getSource();
+                    if (demButtonState.isSelected()) {
+                        data.addSeries(Api.demSeries);
+                    } else {
+                        data.removeSeries(Api.demSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        dieButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent dieButton) {
+                try {
+                    JToggleButton dieButtonState =
+                            (JToggleButton) dieButton.getSource();
+                    if (dieButtonState.isSelected()) {
+                        data.addSeries(Api.dieSeries);
+                    } else {
+                        data.removeSeries(Api.dieSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        gasButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent gasButton) {
+                try {
+                    JToggleButton gasButtonState =
+                            (JToggleButton) gasButton.getSource();
+                    if (gasButtonState.isSelected()) {
+                        data.addSeries(Api.gasSeries);
+                    } else {
+                        data.removeSeries(Api.gasSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        eolButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent eolButton) {
+                try {
+                    JToggleButton eolButtonState =
+                            (JToggleButton) eolButton.getSource();
+                    if (eolButtonState.isSelected()) {
+                        data.addSeries(Api.eolSeries);
+                    } else {
+                        data.removeSeries(Api.eolSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        ccButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ccButton) {
+                try {
+                    JToggleButton ccButtonState =
+                            (JToggleButton) ccButton.getSource();
+                    if (ccButtonState.isSelected()) {
+                        data.addSeries(Api.ccSeries);
+                    } else {
+                        data.removeSeries(Api.ccSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        vapButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent vapButton) {
+                try {
+                    JToggleButton vapButtonState =
+                            (JToggleButton) vapButton.getSource();
+                    if (vapButtonState.isSelected()) {
+                        data.addSeries(Api.vapSeries);
+                    } else {
+                        data.removeSeries(Api.vapSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        fotButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent fotButton) {
+                try {
+                    JToggleButton fotButtonState =
+                            (JToggleButton) fotButton.getSource();
+                    if (fotButtonState.isSelected()) {
+                        data.addSeries(Api.fotSeries);
+                    } else {
+                        data.removeSeries(Api.fotSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        hidButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent hidButton) {
+                try {
+                    JToggleButton hidButtonState =
+                            (JToggleButton) hidButton.getSource();
+                    if (hidButtonState.isSelected()) {
+                        data.addSeries(Api.hidSeries);
+                    } else {
+                        data.removeSeries(Api.hidSeries);
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        // Show button panel
+        buttonPanel.setVisible(true);
+    }
+
+    public void createDatePicker() {
+        // Create a grid layout for the date picker
+        GridLayout grid = new GridLayout(3, 2, 5, 5);
+
+        // Create date picker panel
+        datePickerPanel = new JPanel(grid);
+
+        // Create select button
+        JButton selectPushButton = new JButton("Seleccionar");
+        JButton downloadPushButton = new JButton("Descargar");
+
+        // Create date field with today's date
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        fromDate = dateFormat.format(calendar.getTime());
+        toDate = fromDate;
+
+        // Create date pickers
+        JTextField fromTextField = new JTextField(fromDate);
+        JTextField toTextField = new JTextField(fromDate);
+        DatePicker fromDatePicker = new DatePicker(fromTextField);
+        DatePicker toDatePicker = new DatePicker(toTextField);
+
+        // Create labels
+        JLabel fromLabel = new JLabel("Desde: ");
+        JLabel toLabel = new JLabel("Hasta: ");
+
+        // Add date picker and field to date picker panel
+        datePickerPanel.add(fromLabel);
+        datePickerPanel.add(fromTextField);
+        datePickerPanel.add(toLabel);
+        datePickerPanel.add(toTextField);
+        datePickerPanel.add(selectPushButton);
+        datePickerPanel.add(downloadPushButton);
+
+        // Set picker panel border
+        datePickerPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK),
+                BorderFactory.createEmptyBorder(10, 15, 5, 15)));
+
+        // Show date picker panel
+        datePickerPanel.setVisible(true);
+
+        // Define select button behavior
+        selectPushButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent selectPushButton) {
+                // Clear all time series
+                api.demSeries.clear();
+                api.dieSeries.clear();
+                api.gasSeries.clear();
+                api.eolSeries.clear();
+                api.ccSeries.clear();
+                api.vapSeries.clear();
+                api.fotSeries.clear();
+                api.hidSeries.clear();
+
+                try {
+                    // Get date picker field text
+                    fromDate = fromTextField.getText();
+                    toDate = toTextField.getText();
+
+                    // Make the API call with these two dates
+                    makeApiCall();
+
+                    // Change title accordingly
+                    if (fromDate.equals(toDate)) {
+                        chart.getChart().setTitle("Generación en Canarias (" +
+                                fromDate + ")");
+                    } else {
+                        chart.getChart().setTitle("Generación en Canarias (" +
+                                fromDate + " hasta " + toDate + ")");
+                    }
+                } catch (NumberFormatException ignored) {
+                    // Catch exception
+                }
+            }
+        });
+
+        // Define select button behavior
+        downloadPushButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent downloadPushButton) {
+                try {
+                    ChartUtilities.saveChartAsPNG(new File("download.png"),
+                            chart.getChart(), 1024, 768);
+                } catch (Exception ignored) {
+                    // Catch exception
+                }
+            }
+        });
+    }
+
+    public void makeApiCall() {
+        // Get "from" and "to" dates in Date format
+        LocalDate startDate = LocalDate.parse(fromDate);
+        LocalDate endDate = LocalDate.parse(toDate);
+
+        // iterate over start date to end date
+        LocalDate currentDate = startDate;
+
+        while (currentDate.isBefore(endDate) || currentDate.isEqual(endDate)) {
+            // Reconstruct URL and make an API call
+
+            try {
+                final String url = Api.getUrl(currentDate.toString());
+                Api.download(url, Api.path);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Parse downloaded .json and update TimeSeries objects
+            try {
+                Api.listContent(Api.path);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Add a day to the current date
+            currentDate = currentDate.plusDays(1);
+        }
+    }
+
+    public void createChart() {
+        // Create a flow layout for chart panel
+        GridBagLayout gridBagLayout = new GridBagLayout();
+
+        // Create a white chart panel
+        chartPanel = new JPanel(gridBagLayout);
+        chartPanel.setBackground(Color.WHITE);
+
+        // Set panel border
+        chartPanel.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.BLACK),
+                BorderFactory.createEmptyBorder(10, 15, 5, 15)));
+
+        // Add default graphic
+        try {
+            data = new TimeSeriesCollection();
+            data.addSeries(Api.demSeries);
+
+            // Change title accordingly
+            String title = null;
+            if (fromDate.equals(toDate)) {
+                title = "Generación en Canarias (" + fromDate + ")";
+            } else {
+                title = "Generación en Canarias (" + fromDate + " hasta " +
+                        toDate + ")";
+            }
+            // Create graphic
+            JFreeChart graphic = ChartFactory.createTimeSeriesChart(
+                    title,
+                    "Hora",             // x-axis
+                    "Demanda (MW)",     // y-axis
+                    data,               // time series
+                    true,               // show legend
+                    true,               // use tooltip
+                    false               // generate URLs
+            );
+
+            // Change colors
+//            graphic.getPlot().setBackgroundPaint(Color.WHITE);
+            graphic.setBackgroundPaint(Color.WHITE);
+
+            // Add chart to chart panel
+            chart = new ChartPanel(graphic);
+
+            GridBagConstraints constraints = new GridBagConstraints();
+            // Add the top panel to the JFrame with the specified constraints
+            constraints.gridx = 0;
+            constraints.gridy = 0;
+            constraints.gridwidth = 2;
+            constraints.weightx = 1;
+            constraints.weighty = 0.5;
+            constraints.fill = GridBagConstraints.BOTH;
+            chartPanel.add(chart, constraints);
+
+            // Show chart panel
+            chartPanel.setVisible(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
